007476cdf50971cca6f882aaf01dee8a725f540b
Sebastian Riedel <s@kraih.com>
2023-01-29 02:50:17 +0100

    Rewrite tag tokenizer to be much faster and handle more edge cases

--- src/parser.ts
+++ src/parser.ts
@@ -23,7 +23,6 @@ import {SafeString, escapeRegExp, stickyMatch, xmlUnescape} from '@mojojs/util';
 
 const NAME_RE = new RegExp(`[${NAME_START_CHAR}][${NAME_CHAR}]*`, 'u');
 
-const ATTR_RE = new RegExp(`(${NAME_RE.source}|/)(?:\\s*=\\s*(?:(?:"([^"]*)")|(?:'([^']*)')|([^>\\s]*)))?\\s*`, 'ysu');
 const TEXT_RE = new RegExp(`([^<]+)`, 'ys');
 const DOCTYPE_RE = new RegExp(
   `<!DOCTYPE\\s*(\\w+(?:(?:\\s+\\w+)?(?:\\s+(?:"[^"]*"|'[^']*'))+)?(?:\\s+\\[.+?\\])?\\s*)>`,
@@ -32,7 +31,12 @@ const DOCTYPE_RE = new RegExp(
 const COMMENT_RE = new RegExp(`<!--(.*?)--\\s*>`, 'ys');
 const CDATA_RE = new RegExp(`<!\\[CDATA\\[(.*?)\\]\\]>`, 'ysi');
 const PI_RE = new RegExp(`<\\?(.*?)\\?>`, 'ys');
-const TAG_RE = new RegExp(`<\\s*(\\/)?\\s*(${NAME_RE.source})\\s*((?:${ATTR_RE.source})*?)>`, 'ysu');
+const TAG_ATTR_RE = new RegExp(
+  `\\s*(${NAME_RE.source})(?:\\s*=\\s*(?:(?:"([^"]*)")|(?:'([^']*)')|([^>\\s]*)))?`,
+  'ysu'
+);
+const TAG_END_RE = new RegExp(`\\s*(/)?\\s*>`, 'ys');
+const TAG_START_RE = new RegExp(`<\\s*(\\/)?\\s*(${NAME_RE.source})`, 'ysu');
 const RUNAWAY_RE = new RegExp(`<`, 'y');
 
 export class Parser {
@@ -45,7 +49,8 @@ export class Parser {
     let current: Parent = doc;
 
     const sticky = {offset: 0, value: text};
-    while (text.length > sticky.offset) {
+    const textLength = text.length;
+    while (textLength > sticky.offset) {
       // Text
       const textMatch = stickyMatch(sticky, TEXT_RE);
       if (textMatch !== null) {
@@ -82,60 +87,65 @@ export class Parser {
       }
 
       // Tag
-      const tagMatch = stickyMatch(sticky, TAG_RE);
-      if (tagMatch !== null) {
-        let tag = xml === true ? tagMatch[2] : tagMatch[2].toLowerCase();
+      const before = sticky.offset;
+      const startMatch = stickyMatch(sticky, TAG_START_RE);
+      if (startMatch !== null) {
+        let tag = xml === true ? startMatch[2] : startMatch[2].toLowerCase();
 
-        // Start
-        if (tagMatch[1] === undefined) {
-          const attrs: Record<string, string> = {};
-          const unparsed = tagMatch[3];
-          let close = false;
-          if (unparsed !== undefined) {
-            // Attributes
-            const stickyAttr = {offset: 0, value: unparsed};
-            while (unparsed.length > stickyAttr.offset) {
-              const attrMatch = stickyMatch(stickyAttr, ATTR_RE);
-              if (attrMatch === null) break;
-              const name = xml === true ? attrMatch[1] : attrMatch[1].toLowerCase();
-              if (name === '/') {
-                close = true;
-              } else {
-                attrs[name] = xmlUnescape(attrMatch[2] ?? attrMatch[3] ?? attrMatch[4] ?? '');
+        // Attributes
+        const attrs: Record<string, string> = {};
+        while (textLength > sticky.offset) {
+          const attrMatch = stickyMatch(sticky, TAG_ATTR_RE);
+          if (attrMatch === null) break;
+          const name = xml === true ? attrMatch[1] : attrMatch[1].toLowerCase();
+          attrs[name] = xmlUnescape(attrMatch[2] ?? attrMatch[3] ?? attrMatch[4] ?? '');
+        }
+
+        let close = false;
+        const endMatch = stickyMatch(sticky, TAG_END_RE);
+        if (endMatch !== null) {
+          if (endMatch[1] !== undefined) close = true;
+
+          // Start
+          if (startMatch[1] === undefined) {
+            // "image" is an alias for "img"
+            if (xml === false && tag === 'image') tag = 'img';
+            current = this._start(current, xml, tag, attrs);
+
+            // Element without end tag (self-closing)
+            const isSelfClosing =
+              (xml !== true && EMPTY.has(tag) === true) ||
+              ((xml === true || BLOCK.has(tag) === false) && close === true);
+            if (isSelfClosing === true) current = this._end(current, xml, tag);
+
+            // Raw text elements
+            if (xml === true || (RAW.has(tag) === false && RCDATA.has(tag) === false)) continue;
+            const rawMatch = stickyMatch(sticky, new RegExp(`(.*?)</${escapeRegExp(tag)}(?:\\s+|\\s*>)`, 'ysi'));
+            if (rawMatch === null) continue;
+            const text = RCDATA.has(tag) === true ? xmlUnescape(rawMatch[1]) : rawMatch[1];
+            current.appendChild(new TextNode(new SafeString(text)));
+            current = this._end(current, xml, tag);
+          }
+
+          // End
+          else {
+            // No more content
+            if (xml !== true && NO_MORE_CONTENT[tag] !== undefined) {
+              for (const noMoreContent of NO_MORE_CONTENT[tag]) {
+                current = this._end(current, xml, noMoreContent);
               }
             }
+
+            current = this._end(current, xml, tag);
           }
 
-          // "image" is an alias for "img"
-          if (xml === false && tag === 'image') tag = 'img';
-          current = this._start(current, xml, tag, attrs);
-
-          // Element without end tag (self-closing)
-          const isSelfClosing =
-            (xml !== true && EMPTY.has(tag) === true) || ((xml === true || BLOCK.has(tag) === false) && close === true);
-          if (isSelfClosing === true) current = this._end(current, xml, tag);
-
-          // Raw text elements
-          if (xml === true || (RAW.has(tag) === false && RCDATA.has(tag) === false)) continue;
-          const rawMatch = stickyMatch(sticky, new RegExp(`(.*?)</${escapeRegExp(tag)}(?:\\s+|\\s*>)`, 'ysi'));
-          if (rawMatch === null) continue;
-          const text = RCDATA.has(tag) === true ? xmlUnescape(rawMatch[1]) : rawMatch[1];
-          current.appendChild(new TextNode(new SafeString(text)));
-          current = this._end(current, xml, tag);
+          continue;
         }
 
-        // End
+        // No full tag (reset offset)
         else {
-          // No more content
-          if (xml !== true && NO_MORE_CONTENT[tag] !== undefined) {
-            for (const noMoreContent of NO_MORE_CONTENT[tag]) {
-              current = this._end(current, xml, noMoreContent);
-            }
-          }
-
-          current = this._end(current, xml, tag);
+          sticky.offset = before;
         }
-        continue;
       }
 
       // Runaway "<"
